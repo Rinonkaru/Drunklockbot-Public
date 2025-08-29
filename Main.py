62d3a9271e41828688d4d28c47d10e53971373fe
Cardinal DeVere <Rinonkaru@Proton.me>
2025-08-29 12:09:49 -0500

    Removed `at your option` from license in Main.py

--- Main.py
+++ Main.py
@@ -8,8 +8,7 @@ Copyright (C) 2025 Rinonkaru
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+the Free Software Foundation, either version 3 of the License.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -359,4 +358,5 @@ async def run():
 		
 if __name__ == "__main__":
 	asyncio.run(run())
+
 
